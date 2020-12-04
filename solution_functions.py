4488c5a17002d85c81efb977115e3f34f95505a7
Chris Schon <chris@Chriss-MacBook-Pro.local>
2020-12-04 11:22:48 +0000

    day 3

--- solution_functions.py
+++ solution_functions.py
@@ -1,7 +1,10 @@
+from functools import reduce
+
 def day1_part1(inp):
     """Given a list of expense items, return 
     the multiple of the two items that sum
     to 2020"""
+    
     upper_triangle_sum = [((i,j), inp[i] + inp[j]) 
                           for i in range(len(inp)) 
                           for j in range(i, 200)]
@@ -16,6 +19,7 @@ def day1_part2(inp):
     """Given a list of expense items, return 
     the multiple of 3 items that sum
     to 2020"""
+    
     upper_triangle_sum = [((i,j,k), inp[i] + inp[j] + inp[k]) 
                           for i in range(len(inp)) 
                           for j in range(i, 200)
@@ -45,6 +49,7 @@ def day2_part1(input_day2):
      '4-5 m: mmpth'
     means the password must have 4-5 'm's in,
     but it has 2, so it is not valid"""
+    
     passed_list = [day2_part1_test_single(i) for i in input_day2]
     num_passed = sum(passed_list)
     return num_passed
@@ -68,3 +73,52 @@ def day2_part2(input_day2):
     passed_list = [day2_part2_test_single(i) for i in input_day2]
     num_passed = sum(passed_list)
     return num_passed
+
+def day3_part1(inp):
+    """Given a list representing trees
+    on a taboggan slope:
+    ['....#...............##...#...#.'
+    '#...#..#.....##.##.#.##....#...',
+    '...#.....#...#.................'
+    '#..#..#.......#...#.#..........']
+    calculate the number of trees you
+    would hit as you traverse 3 across
+    and 1 down."""
+    tree_count = 0
+    width = len(inp[0])
+    j = 0
+    for i in range(len(inp)):
+        if inp[i][j] == '#':
+            tree_count += 1
+        j+=3
+        j = j % width
+    return tree_count
+
+
+def day3_part2_solver(inp, right, down):
+    """generalise day3 part 1 for
+    user defined traversal strategy
+    down (steps down) and right (steps right)"""
+    
+    tree_count = 0
+    width = len(inp[0])
+    j = 0
+    for i in range(0, len(inp), down):
+        if inp[i][j] == '#':
+            tree_count += 1
+        j+=right
+        j = j % width
+    return tree_count
+
+
+def day3_part2(inp):
+    """main solver function for day 3 part,
+    returning the product of tree counts
+    of different traversal strategies"""
+    
+    tree_counts = []
+    strats = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    for s in strats:
+        tree_counts.append(day3_part2_solver(inp, s[0], s[1]))
+    tree_counts_multiple = reduce((lambda x, y: x*y), tree_counts)
+    return tree_counts_multiple
