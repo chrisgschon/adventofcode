33927caa7ba2de8b0d09fe9b5f880e6e061272d9
Chris Schon <chris@Chriss-MacBook-Pro.local>
2020-12-06 14:04:46 +0000

    day 6

--- solution_functions.py
+++ solution_functions.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import re
+from collections import Counter
 
 def load_input_as_list(day):
     with open(f"input_data/input_day{day}.txt", "r") as f:
@@ -230,3 +231,24 @@ def day5_part2(inp):
         seatid_range.remove(seatid)
     no_neighbours = [i for i in seatid_range if i > idmin and i < idmax]
     return(no_neighbours)
+
+
+def day6_part1(inp):
+    """return unique counts of groups of
+    strings"""
+    
+    l = ''.join([i if i!='' else '|' for i in inp]).split('|')
+    counts = [len(Counter(i)) for i in l]
+    return(sum(counts))
+
+def day6_part2(inp):
+    """return intersect counts of groups 
+    of strings"""
+    
+    #make list of lists
+    l = [a.strip().split(' ') for a in ' '.join([i if i!='' else '|' for i in inp]).split('|')]
+    
+    #intersect of values for each list
+    intersect_counts = [len(set.intersection(*[set(i) for i in j])) for j in l]
+    
+    return(sum(intersect_counts))
